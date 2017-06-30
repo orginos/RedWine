9b7ce82b1702bed2901195a30f4ffbd7f6c8dfc3
Kostas Orginos <kostas@wm.edu>
2017-06-30 19:59:12 -0400

    upgraded brew

--- axis.rb
+++ axis.rb
@@ -1,7 +1,7 @@
 class Axis < Formula
   homepage 'http://andros.physics.wm.edu/~kostas/axis/'
   url 'http://andros.physics.wm.edu/~kostas/axis/axis-1.3.tar.gz'
-  sha1 '35021974eb0c07a9ce2b6992d7bb0dbd668dd7ca'
+  sha256 '35021974eb0c07a9ce2b6992d7bb0dbd668dd7ca'
 
 #  depends_on :plotutils
  depends_on :tex	
