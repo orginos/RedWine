0c2b8a68935383befa05c3c6812894bb15d0ad83
Kostas Orginos <kostas@wm.edu>
2017-07-08 16:12:09 -0400

    added lhpdf formula again

--- lhapdf.rb
+++ lhapdf.rb
@@ -1,17 +1,37 @@
+# Documentation: http://docs.brew.sh/Formula-Cookbook.html
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
 class Lhapdf < Formula
   desc "C++ LHAPDF libary"
   homepage "http://lhapdf.hepforge.org/install.html"
   url "http://www.hepforge.org/archive/lhapdf/LHAPDF-6.1.6.tar.gz"
-  sha256 "c4604f5d78ad5e498e0128bc3729911e602e0975"
+  sha256 "c28138232e3219bf89cf626c0e414c6291c6b0777c3a931af18e51867657ebe9"
 
-  depends_on :boost
-  
+  # depends_on "cmake" => :build
+
   def install
-    system "./configure",  "--prefix=#{prefix}"
-    system "make", "install"
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
   end
- 
+
   test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test LHAPDF`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
 end
